5ae593ec059f87c72064cba7187f6ecec1c31662
takinabradley <bradfinney98@gmail.com>
2021-11-06 07:04:24 -0400

    declared variables with const for any that applied

--- script.js
+++ script.js
@@ -8,72 +8,72 @@ function Book(title, author, pages, read) { //Object Constructor
 Book.prototype.createCard = function (index) {
   const library = document.querySelector('.library');
   
-  book = document.createElement('article');
+  const book = document.createElement('article');
   book.classList.add('book');
   library.appendChild(book);
   
   
-  bookInfo = document.createElement('div');
+  const bookInfo = document.createElement('div');
   bookInfo.classList.add('bookInfo');
   book.appendChild(bookInfo);
   
-  bookTitle = document.createElement('h2');
+  const bookTitle = document.createElement('h2');
   bookTitle.classList.add('bookTitle');
   bookTitle.textContent = this.title;
   bookInfo.appendChild(bookTitle);
   
-  bookAuthor = document.createElement('span');
+  const bookAuthor = document.createElement('span');
   bookAuthor.classList.add('bookAuthor');
   bookAuthor.textContent = this.author;
   bookInfo.appendChild(bookAuthor);
   
-  bookPages = document.createElement('span');
+  const bookPages = document.createElement('span');
   bookPages.classList.add('bookPages');
   bookPages.textContent = `${this.pages} pages`;
   bookInfo.appendChild(bookPages);
   
   
-  bookButtons = document.createElement('div');
+  const bookButtons = document.createElement('div');
   bookButtons.classList.add('bookButtons');
   book.appendChild(bookButtons);
   
-  readButton = document.createElement('button');
+  const readButton = document.createElement('button');
   readButton.classList.add('readButton');
   if (this.read === 'true') readButton.classList.add('read');
   readButton.textContent= "Read";
   bookButtons.appendChild(readButton);
   
-  deleteButton = document.createElement('button');
+  const deleteButton = document.createElement('button');
   deleteButton.classList.add('deleteButton');
-  deleteButton.setAttribute('data-key', index); //used to remove each book from library
+  deleteButton.setAttribute('data-key', index); //tells delete button index in array
   deleteButton.textContent = 'Delete';
   bookButtons.appendChild(deleteButton);
 }
 
 function createForm() {
-  form = document.querySelector('form');
+  const form = document.querySelector('form');
 
-  titleInput = document.createElement('input');
+  const titleInput = document.createElement('input');
   titleInput.classList.add('titleInput');
   titleInput.setAttribute('placeholder', 'Title');
   form.appendChild(titleInput);
 
-  authorInput = document.createElement('input');
+  const authorInput = document.createElement('input');
   authorInput.classList.add('authorInput');
   authorInput.setAttribute('placeholder', 'author');
   form.appendChild(authorInput);
 
-  pagesInput = document.createElement('input');
+  const pagesInput = document.createElement('input');
   pagesInput.classList.add('pagesInput');
   pagesInput.setAttribute('placeholder', 'pages');
   form.appendChild(pagesInput);
 
-  readInput = document.createElement('input');
+  const readInput = document.createElement('input');
   readInput.classList.add('readInput');
   readInput.setAttribute('placeholder', 'Read? True/False');
   form.appendChild(readInput);
 
-  submitBtn = document.createElement('button');
+  const submitBtn = document.createElement('button');
   submitBtn.classList.add("submitBtn");
   submitBtn.setAttribute('type', 'button');
   submitBtn.textContent = 'Submit';
@@ -88,14 +88,14 @@ function removeForm() {
 function addToLibrary (myLibrary) {
   createForm();
 
-  submitBtn = document.querySelector('.submitBtn');
-  addBtn = document.querySelector('.addBook');
+  const submitBtn = document.querySelector('.submitBtn');
+  const addBtn = document.querySelector('.addBook');
 
   submitBtn.addEventListener('click', (e) => {
-    title = document.querySelector('.titleInput').value;
-    author = document.querySelector('.authorInput').value;
-    pages = document.querySelector('.pagesInput').value;
-    read = document.querySelector('.readInput').value;
+    const title = document.querySelector('.titleInput').value;
+    const author = document.querySelector('.authorInput').value;
+    const pages = document.querySelector('.pagesInput').value;
+    const read = document.querySelector('.readInput').value;
     myLibrary.push(new Book(title, author, pages, read));
     addBtn.classList.toggle('active');
     displayBooks(myLibrary);
@@ -119,7 +119,7 @@ function displayBooks (myLibrary) {
 function allowBookAdding (myLibrary) {
   //enables 'Add Book' button
 
-  addButton = document.querySelector('.addBook');
+  const addButton = document.querySelector('.addBook');
   
   addButton.addEventListener('click', e => {
     if (!addButton.classList.contains('active')) return;
@@ -132,22 +132,22 @@ function allowBookAdding (myLibrary) {
 
 
 function bookButtonInput (myLibrary) {
-  readButtons = document.querySelectorAll('.readButton');
+  const readButtons = document.querySelectorAll('.readButton');
   readButtons.forEach( button => button.addEventListener('click', (e) => { //create a function for this.
     button.classList.toggle('read');
   }));
   
-  deleteButtons = document.querySelectorAll('.deleteButton');
+  const deleteButtons = document.querySelectorAll('.deleteButton');
   deleteButtons.forEach( button => button.addEventListener('click', (e) => {
-    dataKey = e.target.getAttribute('data-key');
-    myLibrary.splice(dataKey, 1);
+    const bookIndex = e.target.getAttribute('data-key');
+    myLibrary.splice(bookIndex, 1);
     displayBooks(myLibrary);
   }));
 }
 
 function init () {
   let myLibrary = [];
-  let Eragon = new Book('Eragon', 'Chris Paolini', 754, 'true');
+  const Eragon = new Book('Eragon', 'Chris Paolini', 754, 'true');
   myLibrary.push(Eragon);
 
   displayBooks(myLibrary);
