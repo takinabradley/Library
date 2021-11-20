1253a63f5db3c3d122190888ef3d0b997173e1dc
takinabradley <bradfinney98@gmail.com>
2021-11-19 19:19:43 -0500

    Library functions and methods are now contained within an object

--- script.js
+++ script.js
@@ -51,139 +51,140 @@ Book.prototype.createCard = function(index) {
 }
 
 
-function createForm() {
-  const form = document.querySelector('form');
-
-  const titleInput = document.createElement('input');
-  titleInput.classList.add('titleInput');
-  titleInput.setAttribute('placeholder', 'Title');
-  form.appendChild(titleInput);
-
-  const authorInput = document.createElement('input');
-  authorInput.classList.add('authorInput');
-  authorInput.setAttribute('placeholder', 'author');
-  form.appendChild(authorInput);
-
-  const pagesInput = document.createElement('input');
-  pagesInput.classList.add('pagesInput');
-  pagesInput.setAttribute('placeholder', 'pages');
-  form.appendChild(pagesInput);
-
-  const readInput = document.createElement('input');
-  readInput.classList.add('readInput');
-  readInput.setAttribute('placeholder', 'Read? True/False');
-  form.appendChild(readInput);
-
-  const submitBtn = document.createElement('button');
-  submitBtn.classList.add("submitBtn");
-  submitBtn.setAttribute('type', 'button');
-  submitBtn.textContent = 'Submit';
-  form.appendChild(submitBtn);
-}
-
-function removeForm() {
-  document.querySelector('form').innerHTML = '';
-}
 
 
-function addToLibrary (myLibrary) {
-  createForm();
+myLibrary = {
+  library: [],
 
-  const submitBtn = document.querySelector('.submitBtn');
-  const addBtn = document.querySelector('.addBook');
-
-  submitBtn.addEventListener('click', (e) => {
-    //console.log(document.querySelector('form').elements);
-    const title = document.querySelector('.titleInput').value;
-    const author = document.querySelector('.authorInput').value;
-    const pages = document.querySelector('.pagesInput').value;
-    const read = document.querySelector('.readInput').value.toLowerCase();
-    myLibrary.push(new Book(title, author, pages, read));
-    addBtn.classList.toggle('active');
-    saveToLocalStorage(myLibrary);
-    displayBooks(myLibrary);
-    removeForm();
-  })
-}
-
-function saveToLocalStorage (myLibrary) {
-  localStorage.setItem('myLibrary', JSON.stringify(myLibrary));
-  if (myLibrary.length === 0) localStorage.clear();
-}
-
-function getFromLocalStorage () {
-  const jsonArray = JSON.parse(localStorage.myLibrary);
-  return jsonArray.map(jsonBook => new Book(jsonBook.title, jsonBook.author, 
-                                            jsonBook.pages, jsonBook.read));
-}
-
-
-function clearBooks () {
-  document.querySelector('.library').innerHTML = '';
-}
-
-function displayBooks (myLibrary) {
-  clearBooks();
-  myLibrary.forEach( (Book, index) => Book.createCard(index));
-  bookButtonInput(myLibrary);
-}
-
-
-function allowBookAdding (myLibrary) {
-  //enables 'Add Book' button
-
-  const addButton = document.querySelector('.addBook');
+  createForm: function() {
+    const form = document.querySelector('form');
   
-  addButton.addEventListener('click', e => {
-    if (!addButton.classList.contains('active')) return;
-    addButton.classList.toggle('active');
-    if (document.querySelector('.library').childElementCount === 15) return;
-    addToLibrary(myLibrary);
-    displayBooks(myLibrary);
-  });
-}
+    const titleInput = document.createElement('input');
+    titleInput.classList.add('titleInput');
+    titleInput.setAttribute('placeholder', 'Title');
+    form.appendChild(titleInput);
+  
+    const authorInput = document.createElement('input');
+    authorInput.classList.add('authorInput');
+    authorInput.setAttribute('placeholder', 'author');
+    form.appendChild(authorInput);
+  
+    const pagesInput = document.createElement('input');
+    pagesInput.classList.add('pagesInput');
+    pagesInput.setAttribute('placeholder', 'pages');
+    form.appendChild(pagesInput);
+  
+    const readInput = document.createElement('input');
+    readInput.classList.add('readInput');
+    readInput.setAttribute('placeholder', 'Read? True/False');
+    form.appendChild(readInput);
+  
+    const submitBtn = document.createElement('button');
+    submitBtn.classList.add("submitBtn");
+    submitBtn.setAttribute('type', 'button');
+    submitBtn.textContent = 'Submit';
+    form.appendChild(submitBtn);
+  },
+  
+  removeForm: function() {
+    document.querySelector('form').innerHTML = '';
+  },
 
+  addToLibrary: function() {
+    this.createForm();
+  
+    const submitBtn = document.querySelector('.submitBtn');
+    const addBtn = document.querySelector('.addBook');
+  
+    submitBtn.addEventListener('click', (e) => {
+      //console.log(document.querySelector('form').elements);
+      const title = document.querySelector('.titleInput').value;
+      const author = document.querySelector('.authorInput').value;
+      const pages = document.querySelector('.pagesInput').value;
+      const read = document.querySelector('.readInput').value.toLowerCase();
+      this.library.push(new Book(title, author, pages, read));
+      addBtn.classList.toggle('active');
+      this.saveToLocalStorage();
+      this.displayBooks();
+      this.removeForm();
+    })
+  },
 
-function bookButtonInput (myLibrary) {
-  const readButtons = document.querySelectorAll('.readButton');
-  const bookIndex = function (e) {
-    return e.target.parentElement.getAttribute('data-key')
-  };
+  saveToLocalStorage: function () {
+    localStorage.setItem('myLibrary', JSON.stringify(this.library));
+    if (this.library.length === 0) localStorage.clear();
+  },
 
-  readButtons.forEach( button => button.addEventListener('click', (e) => {
-    button.classList.toggle('read');
+  getFromLocalStorage: function() {
+    const jsonArray = JSON.parse(localStorage.myLibrary);
+    return jsonArray.map(jsonBook => new Book(jsonBook.title, jsonBook.author, 
+                                              jsonBook.pages, jsonBook.read));
+  },
 
-    if (myLibrary[bookIndex(e)].read === 'true') {
-      myLibrary[bookIndex(e)].read = 'false';
-      saveToLocalStorage(myLibrary);
+  clearBooks: function() {
+    document.querySelector('.library').innerHTML = '';
+  },
+  
+  displayBooks: function() {
+    this.clearBooks();
+    this.library.forEach( (Book, index) => Book.createCard(index));
+    this.bookButtonInput();
+  },
+
+  allowBookAdding: function() {
+    //enables 'Add Book' button
+  
+    const addButton = document.querySelector('.addBook');
+    
+    addButton.addEventListener('click', e => {
+      if (!addButton.classList.contains('active')) return;
+      addButton.classList.toggle('active');
+      if (document.querySelector('.library').childElementCount === 15) return;
+      this.addToLibrary();
+      this.displayBooks();
+    });
+  },
+  
+  
+  bookButtonInput: function() {
+    const readButtons = document.querySelectorAll('.readButton');
+    const bookIndex = function (e) {
+      return e.target.parentElement.getAttribute('data-key')
+    };
+  
+    readButtons.forEach( button => button.addEventListener('click', (e) => {
+      button.classList.toggle('read');
+  
+      if (this.library[bookIndex(e)].read === 'true') {
+        this.library[bookIndex(e)].read = 'false';
+        this.saveToLocalStorage(this.library);
+      } else {
+        this.library[bookIndex(e)].read = 'true';
+        this.saveToLocalStorage(this.library);
+      }
+    })); //toggles button class and toggle actual value of 'read' on book item
+    
+    const deleteButtons = document.querySelectorAll('.deleteButton');
+    deleteButtons.forEach( button => button.addEventListener('click', (e) => {
+      this.library.splice(bookIndex(e), 1);
+      this.saveToLocalStorage(this.library);
+      this.displayBooks(this.library);
+    }));
+  },
+
+  init: function () {
+  
+    if (localStorage.length > 0) {
+      this.library = this.getFromLocalStorage();
     } else {
-      myLibrary[bookIndex(e)].read = 'true';
-      saveToLocalStorage(myLibrary);
+      this.library = [];
+      const bookExample = new Book('Title', 'Author', 120, 'false');
+      this.library.push(bookExample);
     }
-  })); //toggles button class and toggle actual value of 'read' on book item
-  
-  const deleteButtons = document.querySelectorAll('.deleteButton');
-  deleteButtons.forEach( button => button.addEventListener('click', (e) => {
-    myLibrary.splice(bookIndex(e), 1);
-    saveToLocalStorage(myLibrary);
-    displayBooks(myLibrary);
-  }));
-}
-
-
-function init () {
-  let myLibrary = [];
-
-  if (localStorage.length > 0) {
-    myLibrary = getFromLocalStorage();
-  } else {
-    myLibrary = [];
-    const bookExample = new Book('Title', 'Author', 120, 'false');
-    myLibrary.push(bookExample);
+    
+    this.displayBooks(this.library);
+    this.allowBookAdding(this.library);
   }
-  
-  displayBooks(myLibrary);
-  allowBookAdding(myLibrary);
 }
 
-init(); 
+myLibrary.init();
